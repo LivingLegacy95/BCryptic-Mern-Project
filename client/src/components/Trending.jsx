5e8070b7c5fa18c691aaf7bffeb2cb16ae274487
LivingLegacy95 <kgreene012@gmail.com>
2023-11-29 21:04:35 -0800

    Console object in response from useAxios component

--- client/src/components/Trending.jsx
+++ client/src/components/Trending.jsx
@@ -4,14 +4,18 @@ import CoinTrending from './CoinTrending'
 
 
 const Trending = () => {
-    const { response } = useAxios('search/trending');
+    const { response, loading, error } = useAxios('search/trending');
     console.log(response);
+    console.log(error);
     
     return (
         <div className='mt-8'>
             <h1 className='text-2xl mb-2'>Trending</h1>
-            {response && response.coins?.map(coin => <CoinTrending key={coin.item.coin_id}
+            {response && response.map(coin => <CoinTrending key={coin.item.coin_id}
             coin = {coin.item} />)}
+            <p>Is it loading? {loading}</p>
+            <p>Did I get a response? {response}</p>
+            <p>Are there any errors? {error}</p>
         </div>
     )
 }
