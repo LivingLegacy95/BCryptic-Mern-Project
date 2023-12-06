6e262d62bb9170e6ef54b5dce8a5858f0d8e1662
LivingLegacy95 <kgreene012@gmail.com>
2023-12-05 22:53:49 -0800

    Detail Page updated

--- client/src/components/HistoryChart.jsx
+++ client/src/components/HistoryChart.jsx
@@ -1,14 +1,76 @@
 import React from 'react';
 import useAxios from '../hooks/useAxios';
 import { useParams } from 'react-router-dom';
+import React from 'react';
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Filler,
+    Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+import faker from 'faker';
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Filler,
+    Legend
+);
+
+export const options = {
+    responsive: true,
+    plugins: {
+        legend: {
+            position: 'top' as const,
+        },
+        title: {
+            display: true,
+            text: 'Chart.js Line Chart',
+        },
+    },
+};
+
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+export const data = {
+    labels,
+    datasets: [
+        {
+            fill: true,
+            label: 'Dataset 2',
+            data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+            borderColor: 'rgb(53, 162, 235)',
+            backgroundColor: 'rgba(53, 162, 235, 0.5)',
+        },
+    ],
+};
+
+export function App() {
+    return <Line options={options} data={data} />;
+}
+
 
 const HistoryChart = () => {
-    const {id} = useParams();
-    const {response} = useAxios(`coins/${id}?localization=false&tickers=false&market_data=false&community_data=false&developer_data=true&sparkline=false`)
-    
-    if(!response) {
+    const { id } = useParams();
+    const { response } = useAxios(`coins/${id}/market_chart?vs_currency=usd&days=7`)
+    console.log(response)
+
+    if (!response) {
         return <div>Loading... Please Wait</div>
     }
+
+    const coinChartData = response.prices.map(value => ({ x: value[0], y: value[1].toFixed(2) }));
+    console.log(coinChartData)
 
     return (
         <div >
